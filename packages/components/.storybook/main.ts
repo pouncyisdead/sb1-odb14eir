7738282b8e726c836457889ca8fed584ee16deb8
Mike Pouncy <mpouncy@gmail.com>
2024-12-19 11:48:45 -0800

    Update packages/components/.storybook/main.ts [BoltSync]

---
+++ packages/components/.storybook/main.ts
@@ -0,0 +1,19 @@
+import type { StorybookConfig } from '@storybook/web-components-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
